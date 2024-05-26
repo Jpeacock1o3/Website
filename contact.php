86a288b930deeb65153c6ed25730bfbcce746d5c
Jpeacock1o3 <91506771+Jpeacock1o3@users.noreply.github.com>
2024-05-26 17:37:51 -0400

    Add files via upload
    First Commit

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Example of email notification
+    $to = "jadenrpeacock@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+
+    // Alternatively, you can save the data to a database or a file
+}
+
